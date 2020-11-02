65153a9649cd1303efe17a985cae630ddfeb3f9a
Bouwer Botha <Bouwertjie@gmail.com>
2020-11-02 15:19:18 +0200

    1

---
+++ StorageManagementSystem/Controllers/RegistrationController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using StorageManagementSystem.Models;
+
+namespace StorageManagementSystem.Controllers
+{
+    public class RegistrationController : Controller
+    {
+        private readonly ApplicationUser _auc;
+
+        public RegistrationController(ApplicationUser auc)
+        {
+            _auc = auc;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(StudentRegister uc)
+        {
+            _auc.Add(uc);
+            _auc.SaveChanges();
+            ViewBag.message = "You have successfully registered";
+            return View();
+        }
+    }
+}
